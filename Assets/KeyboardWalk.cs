556326c2da90ebc1e234346fe796a6c3486a98a4
elmir <elmir@brab>
2018-10-01 10:54:17 +0200

    initial

---
+++ Assets/KeyboardWalk.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardWalk : MonoBehaviour
+{
+    const float MOVE_SPEED = 3.8f;
+	void Update ()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            move += Vector3.forward;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            move += Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            move += Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;
+        }
+
+        var t = gameObject.transform;
+        move = t.rotation * move * MOVE_SPEED * Time.deltaTime;
+        move.y = 0;
+
+        t.position += move;
+//        print(move * );
+	}
+}
---
+++ Assets/MouseLook.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+///
+/// Adapted from a script by IJM:
+/// http://answers.unity3d.com/questions/29741/mouse-look-script.html
+///
+/// MouseLook rotates the transform based on the mouse delta.
+/// Minimum and Maximum values can be used to constrain the possible rotation
+///
+public class MouseLook : MonoBehaviour
+{
+    public delegate void CurrentMouseRotation(out float xRotation, out float yRotation);
+
+    const float sensitivityX = 6F;
+    const float sensitivityY = 6F;
+
+    const float minimumX = -360F;
+    const float maximumX = 360F;
+
+    const float minimumY = -86F;
+    const float maximumY = 86F;
+
+    float rotationX = 0F;
+    float rotationY = 0F;
+
+    Quaternion originalRotation;
+
+    void Start()
+    {
+        //originalRotation = InputTracking.GetLocalRotation(XRNode.CenterEye);
+        originalRotation = gameObject.transform.rotation;
+    }
+
+    void Update()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        rotationX = ClampAngle(rotationX, minimumX, maximumX);
+        rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+
+        gameObject.transform.rotation = (originalRotation * xQuaternion * yQuaternion);
+//        xRotation = rot.x;
+//        yRotation = rot.y;
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+        {
+            angle += 360F;
+        }
+
+        if (angle > 360F)
+        {
+            angle -= 360F;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+
+}
